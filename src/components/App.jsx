ecd343c06931bf3423586b44cc45f6ebd8152e40
jmjusmin <80433220+jmjusmin@users.noreply.github.com>
2022-07-13 17:04:20 +0700

    Finish event handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [heading, setHeading] = useState("Hello");
+  function isClick() {
+    setHeading("Summitted");
+  }
+
+  const [mouse, setMouse] = useState(false);
+  function isMouseOver() {
+    setMouse(true);
+  }
+  function isMouseOut() {
+    setMouse(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{heading}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: mouse ? "black" : "white" }}
+        onClick={isClick}
+        onMouseOver={isMouseOver}
+        onMouseOut={isMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
